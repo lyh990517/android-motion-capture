fe67927c10ca959cc0839664a86cb2ece617c630
lyh990517 <lyh990517@naver.com>
2025-03-07 16:15:31 +0900

    add finger

--- app/src/main/java/com/yunho/motioncapture/PoseSolver.kt
+++ app/src/main/java/com/yunho/motioncapture/PoseSolver.kt
@@ -73,6 +73,36 @@ class PoseSolverResultWrapper(private val poseSolverResult: PoseSolverResult) {
 //
 //        "左目" to PoseSolverResult::leftEyeRotation,
 //        "右目" to PoseSolverResult::rightEyeRotation
+
+        "左親指１" to PoseSolverResult::leftThumbCMC,
+        "左親指２" to PoseSolverResult::leftThumbMCP,
+        "左人指１" to PoseSolverResult::leftIndexFingerMCP,
+        "左人指２" to PoseSolverResult::leftIndexFingerPIP,
+        "左人指３" to PoseSolverResult::leftIndexFingerDIP,
+        "左中指１" to PoseSolverResult::leftMiddleFingerMCP,
+        "左中指２" to PoseSolverResult::leftMiddleFingerPIP,
+        "左中指３" to PoseSolverResult::leftMiddleFingerDIP,
+        "左薬指１" to PoseSolverResult::leftRingFingerMCP,
+        "左薬指２" to PoseSolverResult::leftRingFingerPIP,
+        "左薬指３" to PoseSolverResult::leftRingFingerDIP,
+        "左小指１" to PoseSolverResult::leftPinkyFingerMCP,
+        "左小指２" to PoseSolverResult::leftPinkyFingerPIP,
+        "左小指３" to PoseSolverResult::leftPinkyFingerDIP,
+
+        "右親指１" to PoseSolverResult::rightThumbCMC,
+        "右親指２" to PoseSolverResult::rightThumbMCP,
+        "右人指１" to PoseSolverResult::rightIndexFingerMCP,
+        "右人指２" to PoseSolverResult::rightIndexFingerPIP,
+        "右人指３" to PoseSolverResult::rightIndexFingerDIP,
+        "右中指１" to PoseSolverResult::rightMiddleFingerMCP,
+        "右中指２" to PoseSolverResult::rightMiddleFingerPIP,
+        "右中指３" to PoseSolverResult::rightMiddleFingerDIP,
+        "右薬指１" to PoseSolverResult::rightRingFingerMCP,
+        "右薬指２" to PoseSolverResult::rightRingFingerPIP,
+        "右薬指３" to PoseSolverResult::rightRingFingerDIP,
+        "右小指１" to PoseSolverResult::rightPinkyFingerMCP,
+        "右小指２" to PoseSolverResult::rightPinkyFingerPIP,
+        "右小指３" to PoseSolverResult::rightPinkyFingerDIP
     )
 
     fun getPoseRotationByIndex(name: String): PoseRotation? {
